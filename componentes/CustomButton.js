de3c6c69a03756276c5c02c027ef1bf364d1e53c
Bruno7a3io <brunogobbato56@gmail.com>
2024-10-30 04:44:14 -0300

    tarjetacomponente

---
+++ componentes/CustomButton.js
@@ -0,0 +1,32 @@
+// CustomButton.js
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+const CustomButton = ({ title, onPress, backgroundColor = '#007BFF', textColor = '#FFFFFF' }) => {
+  return (
+    <TouchableOpacity
+      style={[styles.button, { backgroundColor }]}
+      onPress={onPress}
+    >
+      <Text style={[styles.buttonText, { color: textColor }]}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10,
+    width: '40%', // Ancho del botón
+  },
+  buttonText: {
+    fontSize: 16,
+  },
+});
+
+export default CustomButton;
---
+++ componentes/StarRating.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+const StarRating = () => {
+  const [rating, setRating] = useState(0);
+
+  const handleRating = (newRating) => {
+    setRating(newRating);
+  };
+
+  return (
+    <View style={{ flexDirection: 'column', alignItems: 'center' }}>
+      <View style={{ flexDirection: 'row' }}>
+        {[1, 2, 3, 4, 5].map((star) => (
+          <TouchableOpacity key={star} onPress={() => handleRating(star)}>
+            <FontAwesome
+              name="star"
+              size={15}
+              color={star <= rating ? 'gold' : 'gray'}
+              style={{ marginHorizontal: 5 }}
+            />
+          </TouchableOpacity>
+        ))}
+      </View>
+    </View>
+  );
+};
+
+export default StarRating;
